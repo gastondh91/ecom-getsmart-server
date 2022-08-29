cbf4e054c5e6d48d12af7f006fbdcb301cd8be9c
Gastón d'Hiriart <gastondh91@gmail.com>
2022-08-29 00:09:57 -0300

    min change

--- src/repositories/categories.repository.ts
+++ src/repositories/categories.repository.ts
@@ -6,7 +6,7 @@ export const find = async (): Promise<ICategory[]> => {
   try {
     checkMongoConnection()
     const categoriesResult: ICategory[] = await categoriesModel.find()
-    return categoriesResult.sort((_a, b) => (b.name === 'new' ? -1 : 1))
+    return categoriesResult.sort((_a, b) => (b.name === 'new' ? 1 : -1))
   } catch (error) {
     console.error(error)
     throw error
